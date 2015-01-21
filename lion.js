57bca40bee3aea79a9556d4ca9174bf5530ddcbd
hcz <h.cz@qq.com>
2015-01-21 22:39:21 +0800

    use one file

--- lion.js
+++ lion.js
@@ -91,3 +91,11 @@ lion.addfunc(lionstd, {
         return ast[1];
     },
 });
+
+lion.addfunc(lionstd, {
+    add: function (a, b) {return a + b;},
+    sub: function (a, b) {return a - b;},
+    mul: function (a, b) {return a * b;},
+    div: function (a, b) {return a / b;},
+    mod: function (a, b) {return a % b;},
+}, lion.wrap);
--- lion.math.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-lion.addfunc(lionstd, {
-    add: function (a, b) {return a + b;},
-    sub: function (a, b) {return a - b;},
-    mul: function (a, b) {return a * b;},
-    div: function (a, b) {return a / b;},
-    mod: function (a, b) {return a % b;},
-}, lion.wrap);
