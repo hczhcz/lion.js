0330b3992b313947f20da8c0bdd8b5d6f9eb671d
hcz <h.cz@qq.com>
2015-05-15 15:44:59 +0800

    add repl commands and modify

--- lion.repl.js
+++ lion.repl.js
@@ -1,6 +1,9 @@
 var lion = require('./lion');
+var lion_test = require('./lion_test').test;
 
 process.stdout.write('Lion.js REPL\n\n');
+
+process.stdout.write('> ');
 
 process.stdin.on(
     'readable',
@@ -11,20 +14,43 @@ process.stdin.on(
             var text = input.toString().trim();
 
             if (text !== '') {
-                try {
-                    var result = JSON.stringify(
-                        lion.boot(
-                            JSON.parse(text)
-                        )
-                    );
+                var handlers = {};
 
-                    process.stdout.write(result + '\n');
-                } catch (e) {
-                    process.stdout.write(e.toString() + '\n');
+                handlers['exit'] = handlers['q'] = function () {
+                    process.exit()
+                };
+
+                handlers['help'] = handlers['h'] = function () {
+                    var keys = Object.getOwnPropertyNames(lion.std).sort();
+
+                    process.stdout.write(JSON.stringify(keys) + '\n');
+                };
+
+                if (handlers[text]) {
+                    handlers[text]();
+                } else {
+                    try {
+                        var result = JSON.stringify(
+                            lion.boot(
+                                JSON.parse(text)
+                            )
+                        );
+
+                        process.stdout.write(result + '\n');
+                    } catch (e) {
+                        process.stdout.write(e.toString() + '\n');
+                    }
                 }
             }
-        }
 
-        process.stdout.write('> ');
+            process.stdout.write('> ');
+        }
+    }
+);
+
+process.stdin.on(
+    'end',
+    function () {
+        process.stdout.write('\n');
     }
 );
